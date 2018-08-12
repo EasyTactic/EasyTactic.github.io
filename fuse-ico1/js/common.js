2b602835fe1819ea02063c59ce074852377f0ad0
EasyTactic <36127550+EasyTactic@users.noreply.github.com>
2018-08-12 20:09:26 +0300

    Add files via upload

---
+++ fuse-ico1/js/common.js
@@ -0,0 +1,62 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+jQuery(document).ready(function() {
+	particlesJS.load('particles-js', '/assets/json/particles.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+});
+$(function() {
+		var ViewportHeight = $(window).height();
+    $('.to-top-btn').click(function(){
+       $('html, body').animate({scrollDown:ViewportHeight}, 'slow');
+   });
+});
+$(document).ready(function(){
+    $('.main-screen').on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
